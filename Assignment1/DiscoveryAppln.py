91f73a527369dc462bfe04a33a1b6bb46639f41b
Dan Mopsick <danmopsick@gmail.com>
2023-02-05 15:41:17 -0500

    #1 changing logging level to DEBUG for now

--- Assignment1/DiscoveryAppln.py
+++ Assignment1/DiscoveryAppln.py
@@ -305,7 +305,7 @@ def parseCmdLineArgs ():
 
     parser.add_argument ("-S", "--num_subscribers", type=int, choices=range(1,50), default=1, help="Number of subscribers to build for the system")
 
-    parser.add_argument ("-l", "--loglevel", type=int, default=logging.INFO, choices=[logging.DEBUG,logging.INFO,logging.WARNING,logging.ERROR,logging.CRITICAL], help="logging level, choices 10,20,30,40,50: default 20=logging.INFO")
+    parser.add_argument ("-l", "--loglevel", type=int, default=logging.DEBUG, choices=[logging.DEBUG,logging.INFO,logging.WARNING,logging.ERROR,logging.CRITICAL], help="logging level, choices 10,20,30,40,50: default 20=logging.INFO")
     
     parser.add_argument ("-c", "--config", default="config.ini", help="configuration file (default: config.ini)")
 
