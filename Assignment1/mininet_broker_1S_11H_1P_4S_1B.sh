b62664ef580d0210c56798c74ba674804497cef9
Dan Mopsick <danmopsick@gmail.com>
2023-02-12 12:24:33 -0500

    #1 removing ip address flag i saw on slack that did nothing

--- Assignment1/mininet_broker_1S_11H_1P_4S_1B.sh
+++ Assignment1/mininet_broker_1S_11H_1P_4S_1B.sh
@@ -4,4 +4,4 @@ h3 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 4 -n sub1 -i 60 > sub1.out 2
 h4 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 5 -n sub2 -i 60 > sub2.out 2>&1 &
 h5 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 4 -n sub3 -i 60 > sub3.out 2>&1 &
 h6 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 5 -n sub4 -i 60 > sub4.out 2>&1 &
-h7 ifconfig 10.0.0.7 python3 BrokerAppln.py -d "10.0.0.1:5556"  -a "10.0.0.7" -n broker1 > broker1.out 2>&1 &
+h7 python3 BrokerAppln.py -d "10.0.0.1:5556"  -a "10.0.0.7" -n broker1 > broker1.out 2>&1 &
--- Assignment1/mininet_broker_1S_11H_5P_4S_1B.sh
+++ Assignment1/mininet_broker_1S_11H_5P_4S_1B.sh
@@ -8,4 +8,4 @@ h7 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 4 -n sub1 -i 60 > sub1.out 2
 h8 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 5 -n sub2 -i 60 > sub2.out 2>&1 &
 h9 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 4 -n sub3 -i 60 > sub3.out 2>&1 &
 h10 python3 SubscriberAppln.py -d "10.0.0.1:5556" -T 5 -n sub4 -i 60 > sub4.out 2>&1 &
-h11 ifconfig 10.0.0.11 python3 BrokerAppln.py -d "10.0.0.1:5556"  -a "10.0.0.11" -n broker1 > broker1.out 2>&1 &
+h11 python3 BrokerAppln.py -d "10.0.0.1:5556"  -a "10.0.0.11" -n broker1 > broker1.out 2>&1 &
