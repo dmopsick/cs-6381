4283772f9e95f693b8495b4feb29eb86e8286eee
Dan Mopsick <danmopsick@gmail.com>
2023-02-20 20:28:03 -0500

    #3 taking a stab at the chord table building

--- Assignment2/chord_finger_table.py
+++ Assignment2/chord_finger_table.py
@@ -8,9 +8,11 @@ class FingerTableBuilder():
 
     DHT_KEY = "dht"
 
-    def __init__(self, num_fingers, file_name):
-        self.num_fingers = num_fingers
+    def __init__(self, m, file_name, entity_name, address_space_bytes):
+        self.m = m
         self.file_name = file_name
+        self.entity_name = entity_name
+        self.address_space_bytes = address_space_bytes
 
     def build(self):
         # Load the file 
@@ -31,12 +33,37 @@ class FingerTableBuilder():
         # Sort the DHT list by hash value
         sorted_dht = sorted(dht, key=lambda x: x['hash'])
 
-        print(sorted_dht[-1])
+        # print(sorted_dht[-1])
 
+        entity_index = -1
+
+        # Find the index of the entity we are building the finger table for
+        for index, obj in enumerate(sorted_dht):
+            if obj.id == self.entity_name:
+                entity_index = index
+                break
+
+        # Only proceed with a valid entity that exists in the DHT
+        if entity_index != -1:
+            entity_id = sorted_dht[entity_index].id
+
+
+            # Let's test on finding the next node based on Chord's algorithm
+            i = 1
+
+            # Attempt to use Chord's algorithm
+            next_node_hash = (entity_id + 2^(i + 1)) % self.address_space_bytes)
+
+            # Print here is the hash of next node
+
+            # Need to find the successor of the next node
+
+        else:
+            print("ERROR: Provided entity {} does not exist in the DHT".format(self.entity_name))
 
         
 # Create new finger table builder object
-finger_table_builder = FingerTableBuilder(3, "dht.json")
+finger_table_builder = FingerTableBuilder(3, "dht.json", 'disc1', 48)
 
 # Build the finger table based on the above specified parameters
 finger_table_builder.build()
