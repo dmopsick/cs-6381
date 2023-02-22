cab9ff9ddc1d4adf1dc9099e8448addf6e93779c
Dan Mopsick <danmopsick@gmail.com>
2023-02-22 08:51:35 -0500

    #3 really struggling... boiling down to basic.. i have the data.. able to load the immedaite successor based on next index in the sorted list

--- Assignment2/chord_finger_table.py
+++ Assignment2/chord_finger_table.py
@@ -11,6 +11,7 @@ class FingerTableBuilder():
     def __init__(self):
         self.file_name =  "dht.json"
     
+    # Build the DHT for use in finger table construction
     def build_dht(self):
         # Load the file 
         dht_json_file = open(self.file_name)
@@ -29,21 +30,87 @@ class FingerTableBuilder():
 
         return sorted_dht
 
-    def create_finger_table(self, node_id, m):
+    ##########################################
+    # Create the finger table for a node 
+    #
+    # Use the provided dht.json
+    ##########################################
+    def create_finger_table(self, node_id, m, address_space_bits):
         finger_table = [ ]
+
+        # Load our distributed hash table from json
+        dht = self.build_dht()
+
+        # Should I add successor field to the DHT?
+
+        # print(dht)
+
+        # print (node_id)
         
         # Create m entries in the finger table
         for i in range(m):
-            start = (node_id + (2^i)) % 2^m
+            # start = (node_id + (2^i)) % 2^m
+            start = (node_id + 2^(i-1)) % address_space_bits
+            # print("FLAG 1 ".format(start))
             
+            
+            # We determine the number of the next finger in the table
+            # Now we need to find the successor of that key in the logical ring
+            # successor = self.find_successor(start, dht)
+            # finger_table.append({"start": start, "successor": successor})
 
-            print(start)
+            # print(start)
+
+        # Test that I can load the next node 
+        node = dht[1]
+
+        print(node)
+
+        immediate_successor = self.get_immediate_successor_of_node(node, dht)
+
+        print(immediate_successor)
+        
 
         return finger_table
 
+    def get_immediate_successor_of_node(self, node, dht):
+        successor = None
+
+        # print("FLAG 10")
+        # print(node)
+
+        entity_index = -1
+
+        # print(node['id'])
+
+        # SHOULD I SORT BY ASCENDING EVERY TIME I CHECK
+        # I suppose yes if things were going in out out
+        # But we are fixed for this assignment
+
+        # Find the index of this node in our dht 
+        for index, obj in enumerate(dht):
+            # print(obj)
+            if obj['id'] == node['id']:
+                entity_index = index
+                break
+
+        # print("FLAG 20: {}".format(entity_index))
+        
+        # Check to make sure it is not the last element in the array
+        if entity_index == (len(dht) - 1):
+            # We are working with last node in the logical ring
+            # Point to the first node
+            successor = dht[0]
+        else:
+            # Get the index + 1 as the immediate successor 
+            successor = dht[entity_index + 1]
+       
+        return successor
         
 # Create new finger table builder object
 finger_table_builder = FingerTableBuilder()
 
 # Build the finger table based on the above specified parameters
-finger_table = finger_table_builder.create_finger_table(275867163294784, 3)
+finger_table = finger_table_builder.create_finger_table(11905375445601, 3, 48)
+
+print(finger_table)
