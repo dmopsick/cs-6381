15451fb3c270dae852380f856f15ac53956fe1aa
Dan Mopsick <danmopsick@gmail.com>
2023-03-30 17:35:19 -0400

    HOTFIX 2

--- Assignment3/mininet_broker_5D_1Z_14H_4P_4S_1B.sh
+++ Assignment3/mininet_broker_5D_1Z_14H_4P_4S_1B.sh
@@ -1,4 +1,4 @@
-h1 /home/vboxuser/Downloads/apache-zookeeper-3.7.1-bin/bin/zkServer.sh start-foreground  
+h1 /home/vboxuser/Downloads/apache-zookeeper-3.7.1-bin/bin/zkServer.sh start-foreground &
 h2 python3 DiscoveryAppln.py -P 1 -S 4 > discovery.out 2>&1 &
 h3 python3 PublisherAppln.py -d "10.0.0.2:5556" -a "10.0.0.2" -T 5 -n pub1 > pub1.out 2>&1 &
 h4 python3 SubscriberAppln.py -d "10.0.0.2:5556" -T 4 -n sub1 -i 60 > sub1.out 2>&1 &
